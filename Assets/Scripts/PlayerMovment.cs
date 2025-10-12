03dfffdcf72f5cde0970ad520f71e4a550ba3491
Omer Cohen <omercohen213@gmail.com>
2025-10-12 22:20:15 +0200

    Player Movement

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -1,36 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using UnityEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMovment : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement Settings")]
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float _moveAcceleration = 50f;
+    [SerializeField] private float _maxMoveSpeed = 10f;
+    [SerializeField] private float _gravityScale = 5f;
+    [SerializeField] private float _floatSpeed = 10f; // Upward force while holding up
 
     private Rigidbody2D _rb;
     private Vector2 _moveInput;
-    
+    private bool _isGrounded;
+    [SerializeField] private Transform _groundCheck; // An object on the player to identify ground
+    [SerializeField] private float _groundCheckRadius = 0.1f;
+    [SerializeField] private LayerMask _groundLayer;
+
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _rb.gravityScale = 0f; // We'll handle gravity manually
     }
-  
+
     void Update()
     {
-        // Get input from keyboard or controller
+        // Horizontal and vertical input
         float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        // Normalize to prevent faster diagonal movement
+        float moveY = Input.GetAxisRaw("Vertical"); // Up = float
         _moveInput = new Vector2(moveX, moveY).normalized;
+
+        // Ground check
+        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
     }
 
     void FixedUpdate()
     {
-        // Move the player
-        _rb.velocity = _moveInput * moveSpeed;
+        HandleMovement();
+        ApplyGravity();
+    }
+
+    private void HandleMovement()
+    {
+        // Horizontal movement
+        float targetVelX = _moveInput.x * _maxMoveSpeed;
+        float accelRate = _isGrounded ? _moveAcceleration : _moveAcceleration;
+        float newVelX = Mathf.MoveTowards(_rb.velocity.x, targetVelX, accelRate * Time.fixedDeltaTime);
+
+        // Vertical movement (floating)
+        float newVelY = _rb.velocity.y;
+        if (_moveInput.y > 0) // Holding up
+        {
+            newVelY = _floatSpeed;
+        }
+
+        _rb.velocity = new Vector2(newVelX, newVelY);
+    }
+
+    private void ApplyGravity()
+    {
+        // If player is not holding up, apply gravity
+        if (_moveInput.y <= 0)
+        {
+            float newVelY = _rb.velocity.y - (_gravityScale * 9.81f * Time.fixedDeltaTime);
+            _rb.velocity = new Vector2(_rb.velocity.x, newVelY);
+        }
     }
 }
